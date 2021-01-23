f55b238edc6f7290e7aca7302c83996a59c310f9
Alexandre SALMON <salmon.alexandre10@gmail.com>
2021-01-23 12:13:15 +0100

    database

--- Package.swift
+++ Package.swift
@@ -2,33 +2,35 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kinoswin",
-    platforms: [
-       .macOS(.v10_15)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "App",
-            dependencies: [
-                .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor")
-            ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .target(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
-    ]
+	name: "Kinoswin",
+	platforms: [
+		.macOS(.v10_15)
+	],
+	dependencies: [
+		// 💧 A server-side Swift web framework.
+		.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+		.package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+		.package(url: "https://github.com/ValentinDebon/SQLCodable.git", from: "0.0.2"),
+	],
+	targets: [
+		.target(
+			name: "App",
+			dependencies: [
+				.product(name: "Leaf", package: "leaf"),
+				.product(name: "Vapor", package: "vapor"),
+				.product(name: "SQLCodable", package: "SQLCodable")
+			],
+			swiftSettings: [
+				// Enable better optimizations when building in Release configuration. Despite the use of
+				// the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+				// builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+				.unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+			]
+		),
+		.target(name: "Run", dependencies: [.target(name: "App")]),
+		.testTarget(name: "AppTests", dependencies: [
+			.target(name: "App"),
+			.product(name: "XCTVapor", package: "vapor"),
+		])
+	]
 )
---
+++ create.sql
@@ -0,0 +1,91 @@
+CREATE TABLE users (
+	login TEXT,
+	password_hash TEXT NOT NULL,
+	parent TEXT NOT NULL,
+
+	PRIMARY KEY (login),
+	FOREIGN KEY (parent) REFERENCES users(login)
+);
+
+CREATE TABLE media (
+	uuid TEXT PRIMARY KEY,
+	title TEXT NOT NULL,
+	release_date INTEGER NOT NULL,
+	upload_date INTEGER NOT NULL,
+	synopsis TEXT
+);
+
+CREATE TABLE tags (
+	value TEXT,
+
+	PRIMARY KEY (value)
+);
+
+CREATE TABLE directors (
+	firstname TEXT NOT NULL,
+	lastname TEXT NOT NULL,
+	nationality TEXT NOT NULL,
+	birth_date INTEGER NOT NULL,
+
+	PRIMARY KEY (firstname, lastname)
+);
+
+CREATE TABLE playlists (
+	title TEXT NOT NULL,
+	owner_login TEXT NOT NULL,
+	is_private INTEGER NOT NULL,
+
+	PRIMARY KEY (title, owner_login),
+	FOREIGN KEY (owner_login) REFERENCES users(login)
+);
+
+CREATE TABLE playlist_positions (
+	playlist_title TEXT NOT NULL,
+	playlist_owner_login TEXT NOT NULL,
+	media_uuid TEXT NOT NULL,
+	position INTEGER NOT NULL,
+
+	PRIMARY KEY (playlist_title, playlist_owner_login),
+	FOREIGN KEY (playlist_owner_login, playlist_title) REFERENCES playlists(owner_login, title),
+	FOREIGN KEY (media_uuid) REFERENCES media(uuid)
+); 
+
+CREATE TABLE playlist_tags (
+	playlist_title TEXT NOT NULL,
+	playlist_owner_login TEXT NOT NULL,
+	tag_value TEXT NOT NULL,
+
+	PRIMARY KEY (playlist_title, playlist_owner_login),
+	FOREIGN KEY (playlist_owner_login, playlist_title) REFERENCES playlists(owner_login, title),
+	FOREIGN KEY (tag_value) REFERENCES tags(value)
+);
+
+CREATE TABLE medium_tags (
+	medium_uuid TEXT NOT NULL,
+	tag_value TEXT NOT NULL,
+
+	PRIMARY KEY (medium_uuid, tag_value),
+	FOREIGN KEY (medium_uuid) REFERENCES media(uuid),
+	FOREIGN KEY (tag_value) REFERENCES tags(value)
+);
+
+CREATE TABLE viewing (
+	user_login TEXT NOT NULL,
+	medium_uuid TEXT NOT NULL,
+	date INTEGER NOT NULL,
+
+	PRIMARY KEY (user_login, medium_uuid),
+	FOREIGN KEY (medium_uuid) REFERENCES media(uuid),
+	FOREIGN KEY (user_login) REFERENCES users(login)
+);
+
+CREATE TABLE medium_directors (
+	medium_uuid TEXT NOT NULL,
+	director_firstname TEXT NOT NULL,
+	director_lastname TEXT NOT NULL,
+
+	PRIMARY KEY (medium_uuid),
+	FOREIGN KEY (medium_uuid) REFERENCES media(uuid),
+	FOREIGN KEY (director_firstname, director_lastname) REFERENCES directors(firstname, lastname)
+);
+
