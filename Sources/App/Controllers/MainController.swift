d81bf76e21177797b077fa0ec22ba44cf0175f91
Alexandre SALMON <salmon.alexandre10@gmail.com>
2021-01-26 11:55:30 +0100

    fix title

--- Sources/App/Controllers/MainController.swift
+++ Sources/App/Controllers/MainController.swift
@@ -4,12 +4,27 @@ import Model
 struct MainController: RouteCollection {
 	func boot(routes: RoutesBuilder) throws {
 		routes.get("index", use: index)
+		routes.get("director", use: director)
+		routes.get("movie", use: movie)
+		routes.get("profile", use: profile)
 
 		routes.get(use: indexRedirection)
 	}
 
 	private func index(request: Request) throws -> EventLoopFuture<View> {
 		request.view.render("index")
+	}
+
+	private func director(request: Request) throws -> EventLoopFuture<View> {
+		request.view.render("director")
+	}
+
+	private func movie(request: Request) throws -> EventLoopFuture<View> {
+		request.view.render("movie")
+	}
+
+	private func profile(request: Request) throws -> EventLoopFuture<View> {
+		request.view.render("profile")
 	}
 
 	private func indexRedirection(request: Request) throws -> Response {
